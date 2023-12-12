7a3d7af5fd8d0449fa6d45ce36d7c38c6569d8f6
Ben <budemaso@gmail.com>
2023-12-12 15:42:48 -0700

    Initial main.py (game play window) setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pirate-jump
 Robust 2-D platformer game made using PyGame, sprites, and animations.
+
+I started this game from a tutorial by Clear Code on YouTube for the bones of the project.
+I then added lots of customization and various unique animations.
---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame, sys
+
+# PyGame setup
+pygame.init()
+screen_width = 1200
+screen_height = 700
+screen = pygame.display.set_mode((screen_height, screen_height))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill("black")
+
+    pygame.display.update()
+    clock.tick(60)
