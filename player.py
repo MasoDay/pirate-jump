276f7ccc254476789d7870e39512cfa911265c37
Ben <budemaso@gmail.com>
2023-12-13 12:41:45 -0700

    Loaded all graphics (characters, dust, coins, enemies, etc.) into file structure

--- player.py
+++ player.py
@@ -4,8 +4,10 @@ from support import import_folder
 class Player(pygame.sprite.Sprite):
     def __init__(self, position):
         super().__init__()
-        self.image = pygame.Surface((32, 64))
-        self.image.fill("red")
+        self.import_character_assets()
+        self.frame_index = 0
+        self.animation_speed = 0.15
+        self.image = self.animations["idle"][self.frame_index]
         self.rect = self.image.get_rect(topleft = position)
 
         # Player movement
@@ -15,7 +17,7 @@ class Player(pygame.sprite.Sprite):
         self.jump_speed = -16
 
     def import_character_assets(self):
-        character_path = "../graphics/character/"
+        character_path = "..1 - Basic platformer/graphics/character/"
         self.animations = {"idle": [], "run": [], "jump": [], "fall": []}
 
         for animation in self.animations.keys():
--- support.py
+++ support.py
@@ -1,2 +1,13 @@
+import pygame
+from os import walk
+
 def import_folder(path):
-    pass
+    surface_list = []
+
+    for ___, ___, img_files in walk(path):
+        for image in img_files:
+            full_path = path + "/" + image
+            image_surface = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(image_surface)
+
+    return surface_list
