09666a298c79f5db1297e933bb550d343ebdac26
Ben <budemaso@gmail.com>
2023-12-13 11:04:40 -0700

    Jumping and gravity created

--- level.py
+++ level.py
@@ -1,6 +1,6 @@
 import pygame
 from tiles import Tile
-from settings import tile_size
+from settings import tile_size, screen_width
 from player import Player
 
 class Level:
@@ -32,15 +32,19 @@ class Level:
         player_x = player.rect.centerx
         direction_x = player.direction.x
 
-        if player_x < 200 and direction_x < 0:
+        if player_x < screen_width / 4 and direction_x < 0:
             self.world_shift = 6
             player.speed = 0
-        elif player_x > 1000 and direction_x > 0:
+        elif player_x > screen_width - (screen_width / 4) and direction_x > 0:
             self.world_shift = -6
             player.speed = 0
         else:
             self.world_shift = 0
             player.speed = 6
+
+    def horizontal_movement_collision(self):
+        player = self.player.sprite
+
 
     def run(self):
 
