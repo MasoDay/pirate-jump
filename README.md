5f72857c2838ea0f3b7209d4c4d8b219a18a3336
Ben <budemaso@gmail.com>
2023-12-14 10:13:40 -0700

    1:50:52. Getting a 'too many arguments' error after working on dust particle animation. Next thing to fix

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ Robust 2-D platformer game made using PyGame, sprites, and animations.
 
 I started this game from a tutorial by Clear Code on YouTube for the bones of the project.
 I then added lots of customization and various unique animations.
+
+Day 1 - December 12
+Initial setup, spent a bit of time picking which tutorial to follow and game to build.
+
+Day 2 - December 13
+Worked for 4-5 hours on it, 1.5 of which were spent fixing a file path issue for where my animation images are stored.
+
+Day 2 - December 14
+Started early and making great progress. Collisions, additional animations, feels like player movement is serioisly coming together.
--- player.py
+++ player.py
@@ -5,11 +5,19 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, position):
         super().__init__()
         self.import_character_assets()
+        self.import_dust_run_particles()
+
+        # Logic for animations and selection for each series of stills
         self.frame_index = 0
         self.animation_speed = 0.15
         self.image = self.animations["idle"][self.frame_index]
-
         self.rect = self.image.get_rect(topleft = position)
+
+        # Dust particles and animation timing
+        self.import_dust_run_particles()
+        self.dust_frame_index = 0
+        self.dust_animation_speed = 0.15
+        self.display_surface = surface
 
         # Player movement
         self.direction = pygame.math.Vector2(0, 0)
@@ -20,16 +28,22 @@ class Player(pygame.sprite.Sprite):
         # Player status
         self.status = "idle"
         self.facing_right = True
+        self.on_ground = False
+        self.on_ceiling = False
+        self.on_left = False
+        self.on_right = False
 
     def import_character_assets(self):
         character_path = "./assets/1 - Basic platformer/graphics/character/"
-        # "./graphics/character/" when code and graphics are under the Base Platformer
         self.animations = {"idle": [], "run": [],
                            "jump": [], "fall": []}
 
         for animation in self.animations.keys():
             full_path = character_path + animation
             self.animations[animation] = import_folder(full_path)
+
+    def import_dust_run_particles(self):
+        self.dust_run_particles = import_folder("./assets/1 - Basic platformer/graphics/character/dust_particles/run")
 
     def animate(self):
         animation = self.animations[self.status]
@@ -46,6 +60,21 @@ class Player(pygame.sprite.Sprite):
             flipped_image = pygame.transform.flip(image, True, False)
             self.image = flipped_image
 
+        # Set the rectangle for collisions
+        if self.on_ground and self.on_right:
+            self.rect = self.image.get_rect(bottomright = self.rect.bottomright)
+        elif self.on_ground and self.on_left:
+            self.rect = self.image.get_rect(bottomleft = self.rect.bottomleft)
+        elif self.on_ground:
+            self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
+
+        elif self.on_ceiling and self.on_right:
+            self.rect = self.image.get_rect(topright = self.rect.topright)
+        elif self.on_ceiling and self.on_left:
+            self.rect = self.image.get_rect(topleft = self.rect.topleft)
+        elif self.on_ceiling:
+            self.rect = self.image.get_rect(midtop = self.rect.midtop)
+
     def get_input(self):
         keys = pygame.key.get_pressed()
 
@@ -58,8 +87,9 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE] and self.on_ground:
             self.jump()
+
     def get_status(self):
         if self.direction.y < 0:
             self.status = "jump"
